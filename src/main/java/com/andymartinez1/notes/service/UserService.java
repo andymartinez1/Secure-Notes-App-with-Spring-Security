7a35beb02b3123ad3648c215d94fbad7dc72aa25
Andrew Martinez <andy72391@gmail.com>
2025-02-19 23:24:48 -0500

    Created an admin controller with services and a UserDTO. Added new admin requests to the Postman JSON file.

---
+++ src/main/java/com/andymartinez1/notes/service/UserService.java
@@ -0,0 +1,17 @@
+package com.andymartinez1.notes.service;
+
+import com.andymartinez1.notes.dto.UserDTO;
+import com.andymartinez1.notes.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+
+}
+
---
+++ src/main/java/com/andymartinez1/notes/service/impl/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.andymartinez1.notes.service.impl;
+
+import com.andymartinez1.notes.dto.UserDTO;
+import com.andymartinez1.notes.entity.AppRole;
+import com.andymartinez1.notes.entity.Role;
+import com.andymartinez1.notes.entity.User;
+import com.andymartinez1.notes.repository.RoleRepository;
+import com.andymartinez1.notes.repository.UserRepository;
+import com.andymartinez1.notes.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void updateUserRole(Long userId, String roleName) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        AppRole appRole = AppRole.valueOf(roleName);
+        Role role = roleRepository.findByRoleName(appRole)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    public UserDTO getUserById(Long id) {
+//        return userRepository.findById(id).orElseThrow();
+        User user = userRepository.findById(id).orElseThrow();
+        return convertToDto(user);
+    }
+
+    private UserDTO convertToDto(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.isAccountNonLocked(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isEnabled(),
+                user.getCredentialsExpiryDate(),
+                user.getAccountExpiryDate(),
+                user.getTwoFactorSecret(),
+                user.isTwoFactorEnabled(),
+                user.getSignUpMethod(),
+                user.getRole(),
+                user.getCreatedDate(),
+                user.getUpdatedDate()
+        );
+    }
+
+
+}
