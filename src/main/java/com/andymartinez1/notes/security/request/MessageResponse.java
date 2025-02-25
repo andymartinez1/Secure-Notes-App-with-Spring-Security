13bfdff349328d5d79a47ca895819d690097722f
Andrew Martinez <andy72391@gmail.com>
2025-02-24 19:16:03 -0500

    Created new DTOs and controller endpoints for logging in and registering a user. Updated the Postman collection to include the new endpoints.

---
+++ src/main/java/com/andymartinez1/notes/security/request/MessageResponse.java
@@ -0,0 +1,15 @@
+package com.andymartinez1.notes.security.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+}
